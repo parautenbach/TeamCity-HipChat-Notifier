23604ac3e8104b5cdc4cb755d8a7f548171c6961
parautenbach <parautenbach@gmail.com>
2014-02-28 23:44:50 +0200

    Copyright notices added.

--- src/com/whatsthatlight/teamcity/hipchat/test/HipChatConfigurationControllerTest.java
+++ src/com/whatsthatlight/teamcity/hipchat/test/HipChatConfigurationControllerTest.java
@@ -1,3 +1,19 @@
+/**
+Copyright 2014 Pieter Rautenbach
+
+Licensed under the Apache License, Version 2.0 (the "License");
+you may not use this file except in compliance with the License.
+You may obtain a copy of the License at
+
+  http://www.apache.org/licenses/LICENSE-2.0
+
+Unless required by applicable law or agreed to in writing, software
+distributed under the License is distributed on an "AS IS" BASIS,
+WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+See the License for the specific language governing permissions and
+limitations under the License.
+ */
+
 package com.whatsthatlight.teamcity.hipchat.test;
 
 import static org.junit.Assert.assertEquals;
@@ -13,8 +29,6 @@ import java.io.FileWriter;
 import java.io.IOException;
 import java.net.URISyntaxException;
 
-import javax.servlet.http.HttpServletRequest;
-import javax.servlet.http.HttpServletResponse;
 import javax.xml.parsers.ParserConfigurationException;
 import javax.xml.transform.TransformerException;
 
