e6f829450974a9dc55bba1a77e7a2bab993a8fdd
parautenbach <parautenbach@gmail.com>
2015-02-15 22:06:40 +0200

    Updated emoticons - some old ones removed.

--- src/com/whatsthatlight/teamcity/hipchat/HipChatEmoticonSet.java
+++ src/com/whatsthatlight/teamcity/hipchat/HipChatEmoticonSet.java
@@ -19,7 +19,7 @@ package com.whatsthatlight.teamcity.hipchat;
 public class HipChatEmoticonSet {
 
 	public static final String[] POSITIVE = { "allthethings", "beer", "freddie", "success", "successful", "gangnamstyle" };
-	public static final String[] NEGATIVE = { "tableflip", "boom", "facepalm", "drevil", "failed", "embarrassed", "sadpanda", "poo", "pumpkin", "grumpycat" };
+	public static final String[] NEGATIVE = { "tableflip", "boom", "facepalm", "drevil", "failed", "drool", "sadpanda", "poo", "pumpkin", "grumpycat" };
 	public static final String[] INDIFFERENT = { "badpokerface", "shrug", "unknown" };
 
 }
